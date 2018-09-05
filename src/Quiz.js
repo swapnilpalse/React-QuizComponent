2270448d407bc4e683fc93f58cbcc749b093e0e0
Palse,Swapnil <swapnil.palse@gmail.com>
2018-09-05 12:43:18 -0500

    Completed the module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,17 +8,22 @@ class Quiz extends Component{
     super(props)
     this.state={quiz_position:1}
   }
-
+showNextQuestion(){
+this.setState((state)=>{
+  return {quiz_position:state.quiz_position+1}
+})}
   render(){
-    const isQuizEnd=(this.state.quiz_position-1===quizData.quiz_questions.length);
-    return(
-      <div>
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1 ]}/>
-        <br/>{
 
-        isQuizEnd?
-        <QuizEnd/>:""
-      }
+    const isQuizEnd=(this.state.quiz_position-1 == quizData.quiz_questions.length);
+    console.log(this.state.quiz_position);
+    return(
+      <div>{
+
+      isQuizEnd?
+      <QuizEnd/>:
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
+    }
       </div>
     )
   }
